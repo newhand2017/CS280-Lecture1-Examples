eefe40362320e40a7eef7a6aef26f52cc388ce69
newhand2017 <chivalry19850@gmail.com>
2017-03-03 22:19:04 +0800

    Add Pokemon class

--- CS280-Lecture1-Examples/Program.cs
+++ CS280-Lecture1-Examples/Program.cs
@@ -10,6 +10,20 @@ namespace CS280_Lecture1_Examples
     {
         private static void Main(string[] args)
         {
+            //---------------------------------------------
+            // Claim
+            //---------------------------------------------
+            Pokemon pokemon = new Pokemon();
+
+            pokemon.Height = 2.0f;
+            pokemon.Weight = 18.7f;
+            pokemon.Category = "Lizard";
+            pokemon.Abilities = "Blaze";
+            pokemon.Gender = "M";
+            //---------------------------------------------
+
+            Console.WriteLine("身高:{0}inch, 體重{1}lbs",
+                pokemon.Height, pokemon.Weight);
         }
     }
 }
